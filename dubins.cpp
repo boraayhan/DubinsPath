2048b1c04a8b93a0f5e96ba1580b4a4c5a6e8509
Bora Ayhan <boraayhan3737@gmail.com>
2026-02-25 10:46:08 -0700

    init push, implemented class

---
+++ dubins.cpp
@@ -0,0 +1,19 @@
+#include "dubins.h"
+
+DubinsPath::DubinsPath(Vector3 i, Vector3 f, Vector3 v, double tr) {
+    start = i;
+    end = f;
+    initVelocity = v;
+    turningRadius = tr;
+    timeDelay = 0.0;
+}
+
+DubinsPath::DubinsPath(Vector3 i, Vector3 f, Vector3 v, double tr, double delay) {
+    startPoint = i + v * delay; //TODO: Integrate velocity to avoid assuming constant velocity
+    endPoint = f;
+    initVelocity = v;
+    turningRadius = tr;
+    timeDelay = delay;
+    Vector3 pL;
+    Vector3 pS;
+}
